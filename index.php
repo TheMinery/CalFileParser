0a9faf7715708af172d585b7262d02e020890eb1
Bernard Lechler <administrator@theminery.com>
2018-10-31 10:12:04 -0400

    Corrected link to Meeting Room ICS

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include('./CalFileParser.php');
 $cal = new CalFileParser();
 
 $arCalConference = $cal->parse('https://outlook.office365.com/owa/calendar/c5442ed2c52f432f8a848d9a8975f8d5@theminery.com/4a96e7f80425489e9df4af236e0ee9c07269803195692693419/calendar.ics');
-$arCalMeeting = $cal->parse('https://outlook.office365.com/owa/calendar/70f8d3fed5f64da58f165cb623e8b777@theminery.com/8a77e07cb2984acda13ca1a5e5fcdc6310097253832692424173/calendar.ics');
+$arCalMeeting = $cal->parse('https://outlook.office365.com/owa/calendar/159896db09b34ffb967424656bcbab59@theminery.com/141079b1c7ee4daaa6722fa1925fa95212132363614565888136/calendar.ics');
 $startingDate = strtotime('last monday', strtotime('next sunday'));
 $endingDate = strtotime('+12 days', $startingDate);
 $arConference = getCurrentEvents($arCalConference, $startingDate, $endingDate);
@@ -20,16 +20,19 @@ $arMeeting = getCurrentEvents($arCalMeeting, $startingDate, $endingDate);
 
 //pr($arCal);
 
-$arDates = getCalDates($startingDate);
+//$arDates = getCalDates($startingDate);
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>WRKHUB Conference Room</title>
+<meta http-equiv="refresh" content="300">
 <link rel="stylesheet" type="text/css" href="./css/cfp.css" />
 </head>
 <body>
+<!-- <?php print_r($arCalMeeting); ?> -->
+<!-- <?php print_r($arMeeting); ?> -->
 <section>
     <div id="conference">
     <h1>Conference Room Schedule</h1>
